00ed7ff68d5a87d669349c295b07a3a0ad5a4af2
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 18:14:01 +0530

    Update InsertionSort.java

--- src/com/samples/sorting/InsertionSort.java
+++ src/com/samples/sorting/InsertionSort.java
@@ -1,5 +1,25 @@
 package com.samples.sorting;
 
 public class InsertionSort {
+	
+	
+	//will go backword
+	public void insertionSort(int[] arr) {
+		
+	  for(int i=0; i<=arr.length-1;i++) {
+		  for(int j=i+1;j>0;j--) {
+			  if(arr[i]>arr[j]) {
+				  int temp = arr[j];
+                  arr[j] = arr[j+1];
+                  arr[j+1] = temp;
+			  }
+		  }
+		  
+	  }
+		
+		
+		
+	}
 
 }
+
