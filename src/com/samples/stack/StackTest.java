416cd1be10ff0adffb864433d5c6ca8833b046b8
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 18:11:56 +0530

    Update StackTest.java

--- src/com/samples/stack/StackTest.java
+++ src/com/samples/stack/StackTest.java
@@ -2,4 +2,57 @@ package com.samples.stack;
 
 public class StackTest {
 
+	
+	private int[] arr;
+	
+	private int top;
+	
+	private int size;
+	
+	
+	StackTest(int size){
+		arr=new int[size];
+		top=-1;
+		this.size=size;	
+		
+	}
+	
+	StackTest(){
+		arr=new int[16];
+		top=-1;
+		this.size=size;	
+		
+	}
+	
+	public void push(int value) {
+		if(isFull()) {
+			System.exit(1);
+		}
+		arr[++top]=value;
+	}
+	
+	private boolean isFull() {
+		// TODO Auto-generated method stub
+		return top==size-1;
+	}
+
+	public int pop(int value) {
+		if(isEmpty()) {
+			System.exit(1);
+		}
+		return arr[top--];
+	}
+	public int peek(int value) {
+		if(isEmpty()) {
+			System.exit(1);
+		}
+		return arr[top];
+	}
+	private boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return top==-1;
+	}
+	
+	
+	
 }
